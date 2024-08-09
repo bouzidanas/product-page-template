c933b40450ce14b5139360fe4d7104c26ccedc15
bouzidanas <anasbouzid@gmail.com>
2024-08-09 13:58:14 -0500

    add resource files and new components

--- src/PageSection.tsx
+++ src/PageSection.tsx
@@ -1,50 +1,39 @@
 import './PageSection.css';
 
 interface PageSectionProps {
+    vertical?: boolean;
+    horizontal?: boolean;
     height: "full" | "fit" | string | number;
     maxHeight?: "full" | string | number;
     maxWidth?: "full" | string | number;
     gap?: "none" | "small" | "medium" | "large" | string | number;
     padding?: "none" | "small" | "medium" | "large" | string | number;
     fontScale?: number;
-    stack?: "vertical" | "horizontal";
     backgroundColor?: string | [string, string] | [string, string, string];
+    mode?: "light" | "dark";
     style?: React.CSSProperties;
     children?: React.ReactNode | React.ReactNode[];
 }
 
-const PageSection = ({ height = "fit", maxWidth = 1000, gap = "1.3rem", padding = "3rem", backgroundColor = "transparent", stack="vertical", maxHeight, fontScale, style, children } : PageSectionProps) => {
+const PageSection = ({ height = "fit", gap = "1.3rem", padding = "0", backgroundColor = "transparent", vertical, horizontal, maxWidth, mode, maxHeight, fontScale, style, children } : PageSectionProps) => {
 
     const outerStyle : React.CSSProperties = {
         minHeight: height === "full" ? "100dvh" : height === "fit" ? "fit-content" : "unset",
         height: typeof height === "number" ? `${height}px` : height !== "full" && height !== "fit" ? height : undefined,
         fontSize: fontScale ? `calc(100% * ${fontScale})` : undefined,
-        backgroundColor: Array.isArray(backgroundColor) ? (backgroundColor.length === 2 ? backgroundColor[1] : backgroundColor[2]) : backgroundColor,
+        background: Array.isArray(backgroundColor) ? (backgroundColor.length === 2 ? 'linear-gradient( to bottom,' + backgroundColor[0] + ' 0%, '+ backgroundColor[0] + ' 50%, ' + backgroundColor[1] + ' 50%, ' + backgroundColor[1] + ' 100%)' : (backgroundColor.length === 3 ? 'linear-gradient( to bottom,' + backgroundColor[1] + ' 0%, '+ backgroundColor[1] + ' ' + backgroundColor[0] + ', ' + backgroundColor[2] + ' ' + backgroundColor[0] + ', ' + backgroundColor[2] + ' 100%)' : undefined)) : backgroundColor,
         padding: padding,
     }
     const innerStyle : React.CSSProperties = {
         maxHeight: maxHeight === "full" ? "100%" : maxHeight,
-        maxWidth: maxWidth === "full" ? "100%" : maxWidth,
-        flexDirection: stack === "vertical" ? "column" : stack === "horizontal" ? "row" : undefined,
+        maxWidth: maxWidth === "full" ? undefined : maxWidth,
+        flexDirection: vertical ? "column" : horizontal ? "row" : undefined,
         gap: gap,
         ...style
     }
 
-    const backgroundStyle : React.CSSProperties = {
-        position: "relative", 
-        top: "0", 
-        left: "0", 
-        background: Array.isArray(backgroundColor) ? (backgroundColor!.length === 2 ? backgroundColor[0] : backgroundColor[1]) : backgroundColor, 
-        height: Array.isArray(backgroundColor) ? (backgroundColor!.length === 2 ? "50%" : backgroundColor[0]) : "100%", 
-        width: "100%"
-    }
-
     return (
-        <section className="page__section" style={outerStyle}>
-            {Array.isArray(backgroundColor)
-                ? <div className="section__background" style={backgroundStyle}></div>
-                : null
-            }
+        <section className="page__section" style={outerStyle} data-theme={mode}>
             <div className="section__content" style={innerStyle}>
                 {children}
             </div>
---
+++ src/SubSection.tsx
@@ -0,0 +1,22 @@
+import './SubSection.css';
+
+interface SubSectionProps {
+    horizontal?: boolean;
+    vertical?: boolean;
+    style?: React.CSSProperties;
+    className?: string;
+    children?: React.ReactNode | React.ReactNode[];
+}
+
+const SubSection = ({ horizontal, vertical, style, className, children } : SubSectionProps) => {
+
+    const classString = "content__cont " + (horizontal ? "hor " : vertical ? "vert " : "");
+
+    return (
+        <section className={classString + className} style={style}>
+            {children}
+        </section>
+    )
+}
+
+export default SubSection;
