eb8671ba0adc1e5cf51e6f242f00018024b1041b
bouzidanas <anasbouzid@gmail.com>
2024-08-08 21:57:07 -0500

    add section

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PageSection from './PageSection'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <PageSection backgroundColor="#04182B" height={490} padding={"6.25rem 0 0 0"} style={{textAlign: "center"}}>
+      <h1>Accelerate your app's growth</h1>
+      <p>Your end-to-end solution for every stage of the app marketing journey. Measure, optimize, and scale app growth across platforms.</p>
+      <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <button className="App__top btn-primary">Request a demo</button>
+        <button className="App__top btn-secondary">Learn more</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </PageSection>
     </>
   )
 }
---
+++ src/PageSection.tsx
@@ -0,0 +1,55 @@
+import './PageSection.css';
+
+interface PageSectionProps {
+    height: "full" | "fit" | string | number;
+    maxHeight?: "full" | string | number;
+    maxWidth?: "full" | string | number;
+    gap?: "none" | "small" | "medium" | "large" | string | number;
+    padding?: "none" | "small" | "medium" | "large" | string | number;
+    fontScale?: number;
+    stack?: "vertical" | "horizontal";
+    backgroundColor?: string | [string, string] | [string, string, string];
+    style?: React.CSSProperties;
+    children?: React.ReactNode | React.ReactNode[];
+}
+
+const PageSection = ({ height = "fit", maxWidth = 1000, gap = "2rem", padding = "3rem", backgroundColor = "transparent", stack="vertical", maxHeight, fontScale, style, children } : PageSectionProps) => {
+
+    const outerStyle : React.CSSProperties = {
+        minHeight: height === "full" ? "100dvh" : height === "fit" ? "fit-content" : "unset",
+        height: typeof height === "number" ? `${height}px` : height !== "full" && height !== "fit" ? height : undefined,
+        fontSize: fontScale ? `calc(100% * ${fontScale})` : undefined,
+        backgroundColor: Array.isArray(backgroundColor) ? (backgroundColor.length === 2 ? backgroundColor[1] : backgroundColor[2]) : backgroundColor,
+        padding: padding,
+    }
+    const innerStyle : React.CSSProperties = {
+        maxHeight: maxHeight === "full" ? "100%" : maxHeight,
+        maxWidth: maxWidth === "full" ? "100%" : maxWidth,
+        flexDirection: stack === "vertical" ? "column" : stack === "horizontal" ? "row" : undefined,
+        gap: gap,
+        ...style
+    }
+
+    const backgroundStyle : React.CSSProperties = {
+        position: "relative", 
+        top: "0", 
+        left: "0", 
+        background: Array.isArray(backgroundColor) ? (backgroundColor!.length === 2 ? backgroundColor[0] : backgroundColor[1]) : backgroundColor, 
+        height: Array.isArray(backgroundColor) ? (backgroundColor!.length === 2 ? "50%" : backgroundColor[0]) : "100%", 
+        width: "100%"
+    }
+
+    return (
+        <section className="PageSection" style={outerStyle}>
+            {Array.isArray(backgroundColor)
+                ? <div className="PageSection__background" style={backgroundStyle}></div>
+                : null
+            }
+            <div className="PageSection__content" style={innerStyle}>
+                {children}
+            </div>
+        </section>
+    )
+}
+
+export default PageSection;
