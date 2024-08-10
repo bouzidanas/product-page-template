8f8d6b30a98e4a985e2c6652cfe4fbf838e03931
bouzidanas <anasbouzid@gmail.com>
2024-08-09 23:21:54 -0500

    break JSX into section components

---
+++ src/AdjustLandingPage.tsx
@@ -0,0 +1,227 @@
+import PageSection from './PageSection'
+import SubSection from './SubSection'
+import { HiOutlineCheckCircle } from "react-icons/hi2";
+import './AdjustLandingPage.css'
+
+// const Header = () => {
+
+//   return (
+
+//   )
+// }
+
+const Footer = () => {
+  return ( 
+    <div className="footer">
+      <span>(2024) Product Landing Page Template by Anas Bouzid</span>
+      <a href="#">SECURITY ADVISORIES</a>
+      <a href="#">PRIVACY NOTICE</a>
+      <a href="#">TERMS OF USE</a>
+      <a href="#">CONTACT</a>
+    </div>
+  )
+}
+
+const IntroSection = () => {
+  return (
+    <PageSection vertical centerText maxWidth="1120px" bgColor="#04182B" padding="6.25rem 0 2rem 0" style={{padding: "40px 0", maxWidth: 700, margin: "0 2rem"}}>
+      <h1>Accelerate your app's growth</h1>
+      <p>Your end-to-end solution for every stage of the app marketing journey. Measure, optimize, and scale app growth across platforms.</p>
+      <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: "0.5rem", padding: "1rem 0"}}>
+        <button className="App__top btn-primary">Request a demo</button>
+        <button className="App__top btn-secondary">Learn more</button>
+      </div>
+    </PageSection>
+  )
+}
+
+const ImageTransitionSection = () => {
+  return (
+    <PageSection maxWidth="1120px" bgColor={["33%", "#04182B", "#ffffff"]}>
+      <img src="./imgs/dashboard-app.png" alt="placeholder" style={{width: "85%", height: "auto", borderRadius: "0.7rem"}} />
+    </PageSection>
+  )
+}
+
+const IncentivesSection = () => {
+  return (
+    <PageSection vertical light centerText maxWidth="1120px" bgColor="#ffffff" padding="2rem 0" gap="0.75rem" style={{padding: "45px 0"}}>
+      <SubSection vertical style={{maxWidth: 800, margin: "0 2rem"}}>
+          <h2>Everything you need in one measurement and analytics suite</h2>
+          <p style={{fontSize: "21px"}}>Unlock attribution data, optimize ad performance, and leverage the reporting and insights you need to meet business goals and scale efficiently.</p>
+      </SubSection>
+      <SubSection horizontal style={{width: "min(1400px, 91vw)", padding: "20px 0", flexWrap: "wrap-reverse"}}>
+        <SubSection vertical style={{width: "540px", minWidth: "400px", textAlign: "left"}}>
+          <h3>Maximize impact</h3>
+          <p>Don’t just measure data, measure impact. Drive fast results with accurate, real-time measurement data to meet and exceed your KPIs across channels.</p>
+          <div style={{display: "flex", flexWrap: "wrap", justifyContent: "left", alignItems: "center", padding: "1.3rem 0"}}>
+            <button className="App__top btn-primary">Learn more</button>
+          </div>
+        </SubSection>
+        <SubSection horizontal style={{width: "calc(600px - 2rem)"}}>
+          <img src="./imgs/idea-bulb.png" alt="placeholder" style={{width: "340px", height: "auto", marginLeft: "1rem"}} />
+        </SubSection>
+      </SubSection>
+      <SubSection horizontal style={{width: "min(1400px, 91vw)", padding: "20px 0", flexWrap: "wrap"}}>
+        <SubSection horizontal style={{width: "calc(600px - 2rem)"}}>
+          <img src="./imgs/gain-insights.png" alt="placeholder" style={{width: "340px", height: "auto", marginRight: "1rem"}} />
+        </SubSection>
+        <SubSection vertical style={{width: "540px", minWidth: "400px", textAlign: "left"}}>
+          <h3>Gain insights</h3>
+          <p>Leverage our powerful reporting to easily share customized data visualizations and actionable insights to boost your ROI.</p>
+          <div style={{display: "flex", flexWrap: "wrap", justifyContent: "left", alignItems: "center", padding: "1.3rem 0"}}>
+            <button className="App__top btn-primary">Learn more</button>
+          </div>
+        </SubSection>
+      </SubSection>
+      <SubSection horizontal style={{width: "min(1400px, 91vw)", padding: "20px 0", flexWrap: "wrap-reverse"}}>
+        <SubSection vertical style={{width: "540px", minWidth: "400px", textAlign: "left"}}>
+          <h3>Grow strategically</h3>
+          <p>Experience tangible growth. Scale your business and your profits with ad performance automation, budget optimization, and more.</p>
+          <div style={{display: "flex", flexWrap: "wrap", justifyContent: "left", alignItems: "center", padding: "1.3rem 0"}}>
+            <button className="App__top btn-primary">Learn more</button>
+          </div>
+        </SubSection>
+        <SubSection horizontal style={{width: "calc(600px - 2rem)"}}>
+          <img src="./imgs/celebrate.png" alt="placeholder" style={{width: "340px", height: "auto", marginLeft: "1rem"}} />
+        </SubSection>
+      </SubSection>
+    </PageSection>
+  )
+}
+
+const ProductSection = () => {
+  return (
+    <PageSection vertical maxWidth="1120px" bgColor="#04182B" padding="5rem 0" >
+      <SubSection horizontal style={{width: "min(1400px, 91vw)", padding: "20px 0", flexWrap: "wrap-reverse", gap: "2.6rem"}}>
+        <SubSection vertical style={{flex: "2 0 440px", minWidth: "400px", maxWidth: "calc(670px - 2rem)", textAlign: "left", gap: "0.3rem"}}>
+          <SubSection horizontal style={{gap: "0.6rem", alignItems: "center", marginBottom: "1rem", justifyContent: "left"}}>
+            <img src="./streamlit-logo.svg" alt="Streamlit" style={{width: "38px", height: "auto"}}/>
+            <h4>Streamlit</h4>
+          </SubSection>
+          <h2>Build powerful apps</h2>
+          <p>A faster way to build and share data apps.</p>
+          <div style={{display: "flex", flexWrap: "wrap", justifyContent: "left", alignItems: "center", padding: "2rem 0 0.75rem 0"}}>
+            <button className="App__top btn-primary">Explore Streamlit</button>
+          </div>
+        </SubSection>
+        <SubSection horizontal style={{width: "calc(670px - 2rem)"}}>
+          <video src="./videos/streamlit.mp4" controls style={{width: "100%", height: "auto", borderRadius: "1rem"}} />
+        </SubSection>
+      </SubSection>
+      <SubSection horizontal style={{width: "min(1400px, 91vw)", padding: "20px 0", flexWrap: "wrap", gap: "1.5rem", alignItems: "start"}}>
+        <SubSection horizontal style={{flex: "1 0 346px", minWidth: "320px", maxWidth: "calc(670px - 2rem)", flexWrap: "nowrap", gap: "1rem", alignItems: "start", justifyContent: "left"}}>
+          <HiOutlineCheckCircle color='#ff5454' size={80} style={{marginLeft: "-0.2em", marginTop: "-1.3rem"}}/>
+          <SubSection vertical style={{textAlign: "left", gap: "0.6rem"}}>
+            <h4>Embrace scripting</h4>
+            <p>Build an app in a few lines of code with our magically simple API. </p>
+          </SubSection>
+        </SubSection>
+        <SubSection horizontal style={{flex: "1 0 346px", minWidth: "320px", maxWidth: "calc(670px - 2rem)", flexWrap: "nowrap", gap: "1rem", alignItems: "start", justifyContent: "left"}}>
+          <HiOutlineCheckCircle color='#ff5454' size={80} style={{marginLeft: "-0.2em", marginTop: "-1.3rem"}}/>
+          <SubSection vertical style={{textAlign: "left", gap: "0.6rem"}}>
+            <h4>Embrace scripting</h4>
+            <p>Build an app in a few lines of code with our magically simple API. </p>
+          </SubSection>
+        </SubSection>
+        <SubSection horizontal style={{flex: "1 0 346px", minWidth: "320px", maxWidth: "calc(670px - 2rem)", flexWrap: "nowrap", gap: "1rem", alignItems: "start", justifyContent: "left"}}>
+          <HiOutlineCheckCircle color='#ff5454' size={80} style={{marginLeft: "-0.2em", marginTop: "-1.3rem"}}/>
+          <SubSection vertical style={{textAlign: "left", gap: "0.6rem"}}>
+            <h4>Embrace scripting</h4>
+            <p>Build an app in a few lines of code with our magically simple API. </p>
+          </SubSection>
+        </SubSection>
+      </SubSection>
+    </PageSection>
+  )
+}
+
+const ResourcesSection = () => {
+  return (
+    <PageSection vertical light centerText maxWidth="1120px" bgColor="#e6eef2" padding="2rem 0" gap="0.75rem" style={{padding: "45px 0"}}>
+      <h5 style={{color: "var(--secondary-dark-color)"}}>LEARN FROM OUT EXPERTS</h5>
+      <h2>Whats new at Streamlit?</h2>
+      <SubSection horizontal style={{width: "min(1400px, 91vw)", padding: "20px 0", flexWrap: "wrap", gap: "1.5rem", alignItems: "start"}}>
+        <SubSection vertical style={{flex: "1 0 346px", minWidth: "320px", maxWidth: "calc(670px - 2rem)", flexWrap: "nowrap", gap: "0.6rem", alignItems: "start", justifyContent: "left", textAlign: "left"}}>
+          <div style={{display: "flex", height: "340px", width: "100%", alignItems: "end", justifyContent: "center"}}>
+            <img src="./imgs/mobile.png" alt="placeholder" style={{width: "auto", height: "100%"}} />
+          </div>
+          <p style={{fontWeight: 300, fontSize: "85%", marginBottom: "8px"}}>EBOOK</p>
+          <h4 style={{fontWeight: "500"}}>Mobile app trends: 2024 edition</h4>
+          <p>Get first dibs on exclusive, actionable data, expert insights on AI and personalization, and the keys to supercharged app growth.</p>
+        </SubSection>
+        <SubSection vertical style={{flex: "1 0 346px", minWidth: "320px", maxWidth: "calc(670px - 2rem)", flexWrap: "nowrap", gap: "0.6rem", alignItems: "start", justifyContent: "left", textAlign: "left"}}>
+          <div style={{display: "flex", height: "340px", width: "100%", alignItems: "end", justifyContent: "center"}}>
+            <img src="./imgs/web-dev.png" alt="placeholder" style={{width: "100%", height: "auto", maxHeight: "100%"}} />
+          </div>
+          <p style={{fontWeight: 300, fontSize: "85%", marginBottom: "8px"}}>ARTICLE</p>
+          <h4 style={{fontWeight: "500"}}>Just build it</h4>
+          <p>We don’t want anything to distract you from forward progress. We try our hardest to provide a framework that abstracts away all sorts of engineering details so you can focus on putting the power of data at the fingertips of your stakeholders.</p>
+        </SubSection>
+        <SubSection vertical style={{flex: "1 0 346px", minWidth: "320px", maxWidth: "calc(670px - 2rem)", flexWrap: "nowrap", gap: "0.6rem", alignItems: "start", justifyContent: "left", textAlign: "left"}}>
+          <div style={{display: "flex", height: "340px", alignItems: "end", justifyContent: "center"}}>
+            <img src="./imgs/analysis.png" alt="placeholder" style={{width: "100%", height: "auto", transform: "rotate3d(1, 0, 0, 58deg)", marginBottom: "-2.5rem"}} />
+          </div>
+          <p style={{fontWeight: 300, fontSize: "85%", marginBottom: "8px"}}>EBOOK</p>
+          <h4 style={{fontWeight: "500"}}>The finance app insights report</h4>
+          <p>Propel your finance app UA to new heights with data, insights, and expert analysis. Deep-dive into the fastest-growing regions and verticals in the industry.</p>
+        </SubSection>
+      </SubSection>
+    </PageSection>
+  )
+}
+
+const FooterSection = () => {
+
+  //Footer section with 4 rows of links in a PageSection
+  return (
+    <PageSection vertical centerText maxWidth="1120px" bgColor="#04182B" padding="2rem 0" gap="0.75rem" style={{padding: "45px 0 0 0"}}>
+      <SubSection vertical style={{width: "min(1400px, 91vw)", height: "200px", padding: "20px 0", flexWrap: "wrap", gap: "1.5rem", alignItems: "center"}}>
+        <SubSection vertical style={{flex: "1 0 160px", minWidth: "70px", maxWidth: "calc(670px - 2rem)", flexWrap: "nowrap", gap: "0.6rem", alignSelf: "start", alignItems: "start", justifyContent: "left", textAlign: "left"}}>
+          <h6 style={{color: "var(--secondary-light-color)"}}>SITE</h6>
+          <a href="#">Home</a>
+          <a href="#">Pricing</a>
+          <a href="#">Solutions</a>
+        </SubSection>
+        <SubSection vertical style={{flex: "1 0 160px", minWidth: "70px", maxWidth: "calc(670px - 2rem)", flexWrap: "nowrap", gap: "0.6rem", alignItems: "start", justifyContent: "left", textAlign: "left", marginRight: "3rem"}}>
+          <h6 style={{color: "var(--secondary-light-color)"}}>RESOURCES</h6>
+          <a href="#">Forum</a>
+          <a href="#">Blog</a>
+          <a href="#">Documentation</a>
+          <a href="#">Support</a>
+        </SubSection>
+        <SubSection vertical style={{flex: "1 0 160px", minWidth: "70px", maxWidth: "calc(670px - 2rem)", flexWrap: "nowrap", gap: "0.6rem", alignItems: "start", justifyContent: "left", textAlign: "left"}}>
+          <h6 style={{color: "var(--secondary-light-color)"}}>COMPANY</h6>
+          <a href="#">About</a>
+          <a href="#">Careers</a>
+          <a href="#">Newsroom</a>
+        </SubSection>
+        <SubSection vertical style={{flex: "1 0 160px", minWidth: "70px", maxWidth: "calc(670px - 2rem)", flexWrap: "nowrap", gap: "0.6rem", alignSelf: "end", alignItems: "start", justifyContent: "left", textAlign: "left"}}>
+          <h6 style={{color: "var(--secondary-light-color)"}}>SOCIAL</h6>
+          <a href="#">Twitter/X</a>
+          <a href="#">LinkedIn</a>
+          <a href="#">Facebook</a>
+          <a href="#">Instagram</a>
+          <a href="#">YouTube</a>
+        </SubSection>
+      </SubSection>
+      <Footer />
+    </PageSection>
+  )
+}
+
+function AdjustLandingPage() {
+
+  return (
+    <>
+      <IntroSection />
+      <ImageTransitionSection />
+      <IncentivesSection />
+      <ProductSection />
+      <ResourcesSection />
+      <FooterSection />
+    </>
+  )
+}
+
+export default AdjustLandingPage
--- src/PageSection.tsx
+++ src/PageSection.tsx
@@ -2,38 +2,40 @@ import './PageSection.css';
 
 interface PageSectionProps {
     vertical?: boolean;
-    horizontal?: boolean;
-    height: "full" | "fit" | string | number;
+    light?: boolean;
+    dark?: boolean;
+    centerText?: boolean;
+    height?: "full" | "fit" | string | number;
     maxHeight?: "full" | string | number;
     maxWidth?: "full" | string | number;
     gap?: "none" | "small" | "medium" | "large" | string | number;
     padding?: "none" | "small" | "medium" | "large" | string | number;
     fontScale?: number;
-    backgroundColor?: string | [string, string] | [string, string, string];
-    mode?: "light" | "dark";
+    bgColor?: string | [string, string] | [string, string, string];
     style?: React.CSSProperties;
     children?: React.ReactNode | React.ReactNode[];
 }
 
-const PageSection = ({ height = "fit", gap = "1.3rem", padding = "0", backgroundColor = "transparent", vertical, horizontal, maxWidth, mode, maxHeight, fontScale, style, children } : PageSectionProps) => {
+const PageSection = ({ height = "fit", gap = "1.3rem", padding = "0", bgColor = "transparent", vertical, light, dark, centerText, maxWidth, maxHeight, fontScale, style, children } : PageSectionProps) => {
 
     const outerStyle : React.CSSProperties = {
         minHeight: height === "full" ? "100dvh" : height === "fit" ? "fit-content" : "unset",
         height: typeof height === "number" ? `${height}px` : height !== "full" && height !== "fit" ? height : undefined,
         fontSize: fontScale ? `calc(100% * ${fontScale})` : undefined,
-        background: Array.isArray(backgroundColor) ? (backgroundColor.length === 2 ? 'linear-gradient( to bottom,' + backgroundColor[0] + ' 0%, '+ backgroundColor[0] + ' 50%, ' + backgroundColor[1] + ' 50%, ' + backgroundColor[1] + ' 100%)' : (backgroundColor.length === 3 ? 'linear-gradient( to bottom,' + backgroundColor[1] + ' 0%, '+ backgroundColor[1] + ' ' + backgroundColor[0] + ', ' + backgroundColor[2] + ' ' + backgroundColor[0] + ', ' + backgroundColor[2] + ' 100%)' : undefined)) : backgroundColor,
+        background: Array.isArray(bgColor) ? (bgColor.length === 2 ? 'linear-gradient( to bottom,' + bgColor[0] + ' 0%, '+ bgColor[0] + ' 50%, ' + bgColor[1] + ' 50%, ' + bgColor[1] + ' 100%)' : (bgColor.length === 3 ? 'linear-gradient( to bottom,' + bgColor[1] + ' 0%, '+ bgColor[1] + ' ' + bgColor[0] + ', ' + bgColor[2] + ' ' + bgColor[0] + ', ' + bgColor[2] + ' 100%)' : undefined)) : bgColor,
         padding: padding,
     }
     const innerStyle : React.CSSProperties = {
         maxHeight: maxHeight === "full" ? "100%" : maxHeight,
         maxWidth: maxWidth === "full" ? undefined : maxWidth,
-        flexDirection: vertical ? "column" : horizontal ? "row" : undefined,
+        flexDirection: vertical ? "column" : undefined,
         gap: gap,
+        textAlign: centerText ? "center" : undefined,
         ...style
     }
 
     return (
-        <section className="page__section" style={outerStyle} data-theme={mode}>
+        <section className="page__section" style={outerStyle} data-theme={light ? "light" : dark ? "dark" : undefined}>
             <div className="section__content" style={innerStyle}>
                 {children}
             </div>
