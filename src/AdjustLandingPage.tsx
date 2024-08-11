cc690b1faafeccc8561305341fc3341512b24a7b
bouzidanas <anasbouzid@gmail.com>
2024-08-11 15:28:04 -0500

    improve package organization

--- src/AdjustLandingPage.tsx
+++ src/AdjustLandingPage.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { HiOutlineCheckCircle } from "react-icons/hi2";
 import { FaBars, FaXmark } from "react-icons/fa6";
-import PageSection from './PageSection';
-import SubSection from './SubSection';
+import { PageSection, SubSection } from './components';
+import { useIsMobileWidth, useScrolledPast } from './hooks';
 import './AdjustLandingPage.css';
 
 const ScrollTo = ({dest, onClick=()=>{}, children}: {dest: string, onClick?: ()=>void, children: React.ReactNode}) => {
@@ -12,9 +12,9 @@ const ScrollTo = ({dest, onClick=()=>{}, children}: {dest: string, onClick?: ()=
 }
 
 const Header = () => {
-  const [positionToTop, setPositionToTop] = useState(false);
-  const [mobileMode, setMobileMode] = useState(window.innerWidth <= 752);
   const [showMenu, setShowMenu] = useState(false);
+  const positionToTop = useScrolledPast(75);
+  const mobileMode = useIsMobileWidth(752);
 
   const HPageOptions = () =>
   <>
@@ -23,23 +23,6 @@ const Header = () => {
     <a href="/" onClick={()=>setShowMenu(false)}>Pricing</a>
     <ScrollTo dest='resources' onClick={()=>setShowMenu(false)}>Resources</ScrollTo>
   </>
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setPositionToTop(window.scrollY > 75);
-    };
-    const onResize = () => {
-      setMobileMode(window.innerWidth <= 752);
-    }
-    
-    window.addEventListener("scroll", handleScroll);
-    window.addEventListener("resize", onResize);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      window.removeEventListener("resize", onResize);
-    };
-  }, []);
 
   return ( 
     <>
@@ -113,6 +96,8 @@ const ImageTransitionSection = () => {
 }
 
 const IncentivesSection = () => {
+  const isNarrowView = useIsMobileWidth(1245);
+
   return (
     <PageSection vertical light centerText id="incentives" maxWidth="min(1120px, 90vw)" bgColor="#ffffff" padding="2rem 0" gap="0.75rem" style={{padding: "45px 0"}}>
       <SubSection vertical style={{maxWidth: 800, margin: "0 2rem"}}>
@@ -120,10 +105,10 @@ const IncentivesSection = () => {
           <p style={{fontSize: "21px"}}>Unlock attribution data, optimize ad performance, and leverage the reporting and insights you need to meet business goals and scale efficiently.</p>
       </SubSection>
       <SubSection horizontal style={{padding: "20px 0", flexWrap: "wrap-reverse"}}>
-        <SubSection vertical style={{width: "540px", minWidth: "300px", textAlign: "left"}}>
+        <SubSection vertical style={{width: "540px", minWidth: "300px", textAlign: isNarrowView ? "center" : "left"}}>
           <h3>Maximize impact</h3>
           <p>Don’t just measure data, measure impact. Drive fast results with accurate, real-time measurement data to meet and exceed your KPIs across channels.</p>
-          <div style={{display: "flex", flexWrap: "wrap", justifyContent: "left", alignItems: "center", padding: "1.3rem 0"}}>
+          <div style={{display: "flex", flexWrap: "wrap", justifyContent: isNarrowView ? "center" : "left", alignItems: "center", padding: isNarrowView ? "1.3rem 0 0 0" : "1.3rem 0"}}>
             <button className="App__top btn-primary">Learn more</button>
           </div>
         </SubSection>
@@ -135,19 +120,19 @@ const IncentivesSection = () => {
         <SubSection horizontal style={{width: "calc(600px - 2rem)"}}>
           <img src="./imgs/gain-insights.png" alt="placeholder" style={{width: "340px", height: "auto", marginRight: "1rem"}} />
         </SubSection>
-        <SubSection vertical style={{width: "540px", minWidth: "300px", textAlign: "left"}}>
+        <SubSection vertical style={{width: "540px", minWidth: "300px", textAlign: isNarrowView ? "center" : "left"}}>
           <h3>Gain insights</h3>
           <p>Leverage our powerful reporting to easily share customized data visualizations and actionable insights to boost your ROI.</p>
-          <div style={{display: "flex", flexWrap: "wrap", justifyContent: "left", alignItems: "center", padding: "1.3rem 0"}}>
+          <div style={{display: "flex", flexWrap: "wrap", justifyContent: isNarrowView ? "center" : "left", alignItems: "center", padding: isNarrowView ? "1.3rem 0 0 0" : "1.3rem 0"}}>
             <button className="App__top btn-primary">Learn more</button>
           </div>
         </SubSection>
       </SubSection>
       <SubSection horizontal style={{padding: "20px 0", flexWrap: "wrap-reverse"}}>
-        <SubSection vertical style={{width: "540px", minWidth: "300px", textAlign: "left"}}>
+        <SubSection vertical style={{width: "540px", minWidth: "300px", textAlign: isNarrowView ? "center" : "left"}}>
           <h3>Grow strategically</h3>
           <p>Experience tangible growth. Scale your business and your profits with ad performance automation, budget optimization, and more.</p>
-          <div style={{display: "flex", flexWrap: "wrap", justifyContent: "left", alignItems: "center", padding: "1.3rem 0"}}>
+          <div style={{display: "flex", flexWrap: "wrap", justifyContent: isNarrowView ? "center" : "left", alignItems: "center", padding: isNarrowView ? "1.3rem 0 0 0" : "1.3rem 0"}}>
             <button className="App__top btn-primary">Learn more</button>
           </div>
         </SubSection>
@@ -206,8 +191,10 @@ const ProductSection = () => {
 }
 
 const ResourcesSection = () => {
+  const isNarrorView = useIsMobileWidth(1206);
+
   return (
-    <PageSection vertical light centerText id='resources' maxWidth="1120px" bgColor="#e6eef2" padding="2rem 0" gap="0.75rem" style={{padding: "45px 0 20px 0"}}>
+    <PageSection vertical light centerText id='resources' maxWidth="1120px" bgColor="#e6eef2" padding={isNarrorView ? "2rem 0 0 0" : "2rem 0"} gap="0.75rem" style={{padding: isNarrorView ? "45px 0 0 0" : "45px 0 20px 0"}}>
       <h5 style={{color: "var(--secondary-dark-color)"}}>LEARN FROM OUT EXPERTS</h5>
       <h2>Whats new at Streamlit?</h2>
       <SubSection horizontal style={{width: "min(1400px, 90vw)", padding: "20px 0 0 0", flexWrap: "wrap", gap: "1.5rem", alignItems: "end"}}>
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import PageSection from "./PageSection";
+import SubSection from "./SubSection";
+
+export { PageSection, SubSection };
