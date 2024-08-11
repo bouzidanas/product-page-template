55ffedce571cc65ab10418aa2e13ed714f8899b4
bouzidanas <anasbouzid@gmail.com>
2024-08-11 17:38:41 -0500

    update templete content and readme

--- src/AdjustLandingPage.tsx
+++ src/AdjustLandingPage.tsx
@@ -78,10 +78,10 @@ const Footer = () => {
 const IntroSection = () => {
   return (
     <PageSection vertical centerText maxWidth="700px" bgColor="#04182B" padding="6.25rem 0 2rem 0" style={{padding: "40px 0", margin: "0 2rem"}}>
-      <h1>Accelerate your app's growth</h1>
-      <p>Your end-to-end solution for every stage of the app marketing journey. Measure, optimize, and scale app growth across platforms.</p>
+      <h1>Speed up app development</h1>
+      <p>Build and deploy data apps faster with Streamlit. Streamlit is the fastest way to build, manage, and share data apps.</p>
       <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: "0.5rem", padding: "1rem 0"}}>
-        <button className="App__top btn-primary">Request a demo</button>
+        <button className="App__top btn-primary">Try Streamlit now</button>
         <button className="App__top btn-secondary">Learn more</button>
       </div>
     </PageSection>
