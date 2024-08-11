3a6e5430c49cf0d3aba69045e93892c78a728fa5
bouzidanas <anasbouzid@gmail.com>
2024-08-11 12:24:30 -0500

    update links

--- src/AdjustLandingPage.tsx
+++ src/AdjustLandingPage.tsx
@@ -20,7 +20,7 @@ const Header = () => {
   <>
     <ScrollTo dest='products' onClick={()=>setShowMenu(false)}>Products</ScrollTo>
     <ScrollTo dest='incentives' onClick={()=>setShowMenu(false)}>Solutions</ScrollTo>
-    <a href="#" onClick={()=>setShowMenu(false)}>Pricing</a>
+    <a href="/" onClick={()=>setShowMenu(false)}>Pricing</a>
     <ScrollTo dest='resources' onClick={()=>setShowMenu(false)}>Resources</ScrollTo>
   </>
 
@@ -46,7 +46,7 @@ const Header = () => {
       <PageSection vertical centerText light={positionToTop} className={"page__nav " + (positionToTop ? "latch-top" : "")} height="100px" maxWidth="1120px" bgColor={positionToTop ? "#fff" : "#04182B"} style={{padding: "22px 0"}}>
         <SubSection horizontal className='header' style={{width: "min(1400px, 90vw)", gap: "2rem", alignItems: "center", justifyContent: "space-between", fontWeight: "500"}}>
           <SubSection horizontal className="left">
-            <a href="#"><span style={{fontSize: "240%"}}>PLPT</span></a>
+            <a href="/"><span style={{fontSize: "240%"}}>PLPT</span></a>
             {mobileMode || <HPageOptions />}
           </SubSection>
           {mobileMode 
@@ -54,7 +54,7 @@ const Header = () => {
                 <FaBars size={32} onClick={()=>setShowMenu(true)}/>
               </SubSection>
             : <SubSection horizontal className="right">
-                <a href="#">Login</a>
+                <a href="/">Login</a>
                 <button className="App__top btn-primary">Sign up</button>
               </SubSection>
           }
@@ -64,7 +64,7 @@ const Header = () => {
         <PageSection vertical centerText light className="page__nav mobile latch-top"  height="100lvh" maxWidth="1120px" bgColor= "#fff" style={{justifyContent: "start", height: "100%"}}>
           <SubSection vertical className='header' style={{width: "min(1400px, 90vw)", gap: "0rem", alignItems: "start"}}>
             <SubSection horizontal style={{width: "100%",justifyContent: "space-between"}}>
-              <a href="#"><span style={{fontSize: "240%"}}>PLPT</span></a>
+              <a href="/"><span style={{fontSize: "240%"}}>PLPT</span></a>
               <FaXmark size={32} onClick={()=>setShowMenu(false)}/>
             </SubSection>
             <HPageOptions />
